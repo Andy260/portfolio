47f8df5a4944fcf89c77486c49620aa23ea8ae49
Andrew Barbour <andrew.barbour1@gmail.com>
2022-02-24 18:16:06 +1100

    Production builds of core client projects now minify compiled JavaScript files and omit source maps

--- src/Client/Core/gulpfile.ts
+++ src/Client/Core/gulpfile.ts
@@ -1,7 +1,12 @@
 ﻿const { src, dest } = require("gulp");
 const del = require("del");
+const filelog = require("gulp-filelog");
+const gulpif = require("gulp-if");
 const sourcemaps = require("gulp-sourcemaps");
 const ts = require("gulp-typescript");
+const uglify = require("gulp-uglify");
+
+let isProduction: boolean = process.env.NODE_ENV === "production"; 
 
 // Compiles all Typescript files
 function compileTypescript(): void
@@ -10,17 +15,19 @@ function compileTypescript(): void
     let tsProject = ts.createProject("tsconfig.json");
     // Create pipe for compilation
     let tsResult = src(["**/*.ts", "!node_modules/**", "!gulpfile.ts", "!wwwroot/**"])
-        .pipe(sourcemaps.init())
+        .pipe(filelog())
+        .pipe(gulpif(!isProduction, sourcemaps.init()))
         .pipe(tsProject());
 
-    return tsResult.js.pipe(sourcemaps.write("./"))
+    return tsResult.js.pipe(gulpif(!isProduction, sourcemaps.write("./")))
+        .pipe(gulpif(isProduction, uglify()))
         .pipe(dest("./"));
 }
 
 // Removes all compiled JavaScript files
 function cleanTypescript(): void
 {
-    return del(["**/*.js", "!node_modules/**", "!gulpfile.js", "!gulpfile.ts"]);
+    return del(["**/*.js" , "**/*js.map", "!node_modules/**", "!gulpfile.js", "!gulpfile.ts"]);
 }
 
 exports.compileTypescript = compileTypescript;
