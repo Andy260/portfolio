4614e45e963c4b305af206605b56af69d3211cd0
Andrew Barbour <andrew.barbour1@gmail.com>
2022-02-24 20:41:43 +1100

    Core client project now compiles SCSS files

--- src/Client/Core/gulpfile.ts
+++ src/Client/Core/gulpfile.ts
@@ -1,15 +1,35 @@
-﻿const { src, dest } = require("gulp");
+﻿const { src, dest, series, parallel } = require("gulp");
+const cleancss = require("gulp-clean-css");
 const del = require("del");
 const filelog = require("gulp-filelog");
 const gulpif = require("gulp-if");
+const sass = require("gulp-dart-sass");
 const sourcemaps = require("gulp-sourcemaps");
 const ts = require("gulp-typescript");
 const uglify = require("gulp-uglify");
 
 let isProduction: boolean = process.env.NODE_ENV === "production"; 
 
+let scssSettings =
+{
+    includePaths: ["node_modules/"],
+    outputStyle: "expanded"
+};
+
+// Compiles all SCSS files
+function compileSCSS(): any
+{
+    return src(["**/*.scss", "!node_modules/**", "!wwwroot/**"])
+        .pipe(filelog())
+        .pipe(gulpif(!isProduction, sourcemaps.init()))
+        .pipe(sass(scssSettings).on("error", sass.logError))
+        .pipe(gulpif(isProduction, cleancss({ level: 2 })))
+        .pipe(gulpif(!isProduction, sourcemaps.write("./")))
+        .pipe(dest("./"));
+}
+
 // Compiles all Typescript files
-function compileTypescript(): void
+function compileTypescript(): any
 {
     // Create Typescript project
     let tsProject = ts.createProject("tsconfig.json");
@@ -24,11 +44,35 @@ function compileTypescript(): void
         .pipe(dest("./"));
 }
 
-// Removes all compiled JavaScript files
-function cleanTypescript(): void
+// Copies any dependency files into the project
+function restoreDependencies(): any
 {
-    return del(["**/*.js" , "**/*js.map", "!node_modules/**", "!gulpfile.js", "!gulpfile.ts"]);
+    return src("node_modules/open-iconic/font/fonts/*.*")
+        .pipe(dest("wwwroot/css/open-iconic/font/fonts"));
 }
 
+// Removes any dependency files from the project
+function cleanDependencies(): any
+{
+    return del(["wwwroot/css/open-iconic/font/fonts"]);
+}
+
+// Removes all compiled CSS files
+function cleanSCSS(): any
+{
+    return del(["**/*.css", "**/*.css.map", "!node_modules/**"])
+}
+
+// Removes all compiled JavaScript files
+function cleanTypescript(): any
+{
+    return del(["**/*.js" , "**/*.js.map", "!node_modules/**", "!gulpfile.js", "!gulpfile.ts"]);
+}
+
+exports.build =
+    series(parallel(cleanDependencies, cleanSCSS, cleanTypescript),
+        parallel(restoreDependencies, compileSCSS, compileTypescript));
+exports.compileSCSS = compileSCSS;
 exports.compileTypescript = compileTypescript;
-exports.clean = cleanTypescript;
+exports.restoreDependencies = restoreDependencies;
+exports.clean = parallel(cleanDependencies, cleanSCSS, cleanTypescript);
