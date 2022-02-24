4981249d9810294928488d5c3131d38aff4d74c1
Andrew Barbour <andrew.barbour1@gmail.com>
2022-02-24 17:32:20 +1100

    Core client project now compiles Typescript files

---
+++ src/Client/Core/gulpfile.ts
@@ -0,0 +1,27 @@
+﻿const { src, dest } = require("gulp");
+const del = require("del");
+const sourcemaps = require("gulp-sourcemaps");
+const ts = require("gulp-typescript");
+
+// Compiles all Typescript files
+function compileTypescript(): void
+{
+    // Create Typescript project
+    let tsProject = ts.createProject("tsconfig.json");
+    // Create pipe for compilation
+    let tsResult = src(["**/*.ts", "!node_modules/**", "!gulpfile.ts", "!wwwroot/**"])
+        .pipe(sourcemaps.init())
+        .pipe(tsProject());
+
+    return tsResult.js.pipe(sourcemaps.write("./"))
+        .pipe(dest("./"));
+}
+
+// Removes all compiled JavaScript files
+function cleanTypescript(): void
+{
+    return del(["**/*.js", "!node_modules/**", "!gulpfile.js", "!gulpfile.ts"]);
+}
+
+exports.compileTypescript = compileTypescript;
+exports.clean = cleanTypescript;
